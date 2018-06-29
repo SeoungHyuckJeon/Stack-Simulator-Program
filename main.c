a5e8a0042dbfe2c858f49697a4af617481304a90
SeoungHyuck Jeon <40155007+SeoungHyuckJeon@users.noreply.github.com>
2018-06-29 18:56:30 +0900

    ssp third day
    showStack, push, pop modified.
    
    searchdata, isEmpty entered

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #define SIZE 5;								//스택 크기
 
 int top;									//스택을 가리키는 위치
+int stack[SIZE];
 
 void clearScreen()							//화면지우개
 {
@@ -11,7 +12,7 @@ void clearScreen()							//화면지우개
 
 void wait()									//키누르기 전까지 대기
 {
-	printf("     아무키나 누르세요.\n");
+	printf("     Press any key\n");
 	fflush(stdin);
 	getchar();
 }
@@ -64,50 +65,55 @@ int isnegative(int)						//매개 변수로 받은 정수가 음수인지 판별
 
 int isEmpty()							//가리키는 스택이 비어있는지 확인
 {
-
+	if (stack[top - 1] == NULL) return 1;
+	else return 0;
 }
 
 int isFull()							//스택이 가득 찼는지 확인
 {
-	int top;
 	top == SIZE - 1;
 
-	if (stack(top) == NULL) return 0;
+	if (stack[top] == NULL) return 0;	//비어있음 0 아니면 1 리턴
 	else return 1;
 }
 
 int push(int)							//입력받은 정수 스택에 넣기
 {
-	scanf_s("%d", &stack(top));
+	scanf_s("%d", &stack[top]);
 	isnegative(int stack[top]);
 	if (isnegative == 0)
 	{
 		puts("Error : You entered negative number, Please Enter positive number.");
 		stack[top] = 0;
+		return 0;						//넣었으면 1, 못넣었으면 0 리턴
 	}
-	else top++;
+	else
+	{
+		top++;
+		return 1;
+	}
 }
 
 int pop()								//가리키는 스택 내용 출력
 {
 	top--;
-	if (top < (-1))
+	if (stack[top] < (-1))
 	{
 		puts("Error : Stack is empty.");
-		top = top - 1;
-		return -1;
+		top = top + 1;
+		return -1;						//꺼냈으면 가리키는 스택 값, 못꺼냈으면 -1 리턴 
 	}
 	else
 	{
 		printf("%d\n", stack[top + 1]);
-		return 0;
+		return stack[top-1];
 	}
 
 }
 
 void showStack()						//스택 모든 내용 출력
 {
-	if (isEmpty() = 1)
+	if (stack[top]==(-1))
 		puts("Stack is Empty.");
 	else
 	{
@@ -123,9 +129,32 @@ void showStack()						//스택 모든 내용 출력
 	}
 }
 
-void searchData(int)
+void searchData(int)					//특정 값 찾기
 {
 	int i;
+	if (isEmpty() == 1)
+	{
+		puts("Stack is Empty");
+	}
+	else
+	{
+		printf("Enter the search data : ");
+		scanf_s("%d", &i);
+		for (;;)
+		{
+			top--;
+			if (stack[top] == i);
+			{
+				printf("%d is entered at stack %d\n", i, top + 1);
+				break;
+			}
+			else if (top == (-1))
+			{
+				puts("No found");
+				break;
+			}
+		}
+	}
 }
 
 void main()
